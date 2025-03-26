bde66502f2dd7a5e19a186ce6b36736078212728
giyushino <allanzhang440@gmail.com>
2025-03-25 17:35:16 -0700

    fish update

--- fish/config.fish
+++ fish/config.fish
@@ -20,3 +20,53 @@ function conda
     end
 end
 
+
+function vpn  
+    cd /opt/cisco/secureclient/bin/ 
+    ./vpnui 
+end
+ 
+function config  
+    cd /home/allan/.config     
+    nvim
+end
+
+function face
+    set options "ᕦʕ •`ᴥ•´ʔᕤ fishin!" "₍ᐢ•ﻌ•ᐢ₎* what we catchin?" "(≖͞_≖̥) we still have work?" "‧º·(˚ ˃̣̣̥⌓˂̣̣̥ )‧º· i'm tired!" "ᕕ( ╯°□° )ᕗ hurry up!" "(⌣̀_⌣́) fine" "ʕ´• ᴥ•̥`ʔ bearo?" "( •_•) really?" "(˚ ˃̣̣̥⌓˂̣̣̥ ) why me!"   
+    set random_option (shuf -e $options -n 1)
+    echo $random_option
+end
+
+
+function useful
+    cd .config 
+    cat useful.txt
+end
+
+
+function llm
+    /home/allan/miniconda3/envs/llm/bin/python /home/allan/nvim/python/llm.py
+end
+
+function llama
+    ollama run llama3.2:1b
+end
+
+function gemma
+    ollama run gemma2:9b
+end
+
+function qwen2.5
+    ollama run qwen2.5
+end
+
+function qwen7
+    ollama run qwen:7b
+end
+
+function qwen4
+    ollama run qwen:4b
+end
+set -x LS_COLORS "di=36"  # Light blue for directories
+
+
