d5dd6aa99bb4e21e2aa9ec5bee2eaad408430e74
giyushino <allanzhang440@gmail.com>
2025-12-19 23:35:18 -0800

    update bash script

--- colors.sh
+++ colors.sh
@@ -2,44 +2,57 @@
 
 COLOR=$1
 
-echo setting color to $COLOR
+echo "Setting color to $COLOR"
 BASE="/home/allan/.config"
 
-#/home/allan/.config/hypr/hyprland.conf
-
-if [ $COLOR == "blue" ]; then
-    cp $BASE/fish/config_blue.fish $BASE/fish/config.fish
-    cp $BASE/fish/fish_variables_blue $BASE/fish/fish_variables
+if [ "$COLOR" == "blue" ]; then
+    # Fish
+    cp $BASE/fish/blue.fish $BASE/fish/conf.d/colors.fish
     cp $BASE/fish/functions/blue.fish $BASE/fish/functions/fish_prompt.fish
-    cp $BASE/waybar/blue.css $BASE/waybar/style.css
-    cp $BASE/hypr/blue.conf $BASE/hypr/hyprland.conf 
-    cp $BASE/kitty/blue.conf $BASE/kitty/kitty.conf
-    cp $BASE/nvim/blue/init.lua $BASE/nvim/init.lua
-    cp $BASE/nvim/blue/theme.lua $BASE/nvim/lua/plugins/theme.lua
-    cp $BASE/nvim/blue/lualine.lua $BASE/nvim/lua/plugins/lualine.lua
 
+    # Waybar
+    cp $BASE/waybar/blue.css $BASE/waybar/colors.css
+
+    # Hyprland
+    cp $BASE/hypr/blue.conf $BASE/hypr/colors.conf
+
+    # Kitty
+    cp $BASE/kitty/blue.conf $BASE/kitty/kitty.conf
+
+    # Neovim
+    cp $BASE/nvim/lua/colors/blue.lua $BASE/nvim/lua/colors/init.lua
+
+    # Wallpaper
     hyprctl hyprpaper preload /home/allan/Downloads/images/aquarium_reformat.jpg
     hyprctl hyprpaper wallpaper "eDP-1,/home/allan/Downloads/images/aquarium_reformat.jpg"
     hyprctl hyprpaper wallpaper "DP-1,/home/allan/Downloads/images/aquarium_reformat.jpg"
+    hyprctl hyprpaper wallpaper "HDMI-A-1,/home/allan/Downloads/images/aquarium_reformat.jpg"
 
 else
-    cp $BASE/fish/config_black.fish $BASE/fish/config.fish
-    cp $BASE/fish/fish_variables_black $BASE/fish/fish_variables
+    # Fish
+    cp $BASE/fish/black.fish $BASE/fish/conf.d/colors.fish
     cp $BASE/fish/functions/black.fish $BASE/fish/functions/fish_prompt.fish
-    cp $BASE/waybar/black.css $BASE/waybar/style.css
-    cp $BASE/hypr/black.conf $BASE/hypr/hyprland.conf 
-    cp $BASE/kitty/black.conf $BASE/kitty/kitty.conf
-    cp $BASE/nvim/black/init.lua $BASE/nvim/init.lua
-    cp $BASE/nvim/black/theme.lua $BASE/nvim/lua/plugins/theme.lua
-    cp $BASE/nvim/black/lualine.lua $BASE/nvim/lua/plugins/lualine.lua
 
-    hyprctl hyprpaper preload /home/allan/Downloads/images/ed1.jpg
-    hyprctl hyprpaper wallpaper "eDP-1,/home/allan/Downloads/images/ed1.jpg"
-    hyprctl hyprpaper wallpaper "DP-1,/home/allan/Downloads/images/ed1.jpg"
+    # Waybar
+    cp $BASE/waybar/black.css $BASE/waybar/colors.css
+
+    # Hyprland
+    cp $BASE/hypr/black.conf $BASE/hypr/colors.conf
+
+    # Kitty
+    cp $BASE/kitty/black.conf $BASE/kitty/kitty.conf
+
+    # Neovim
+    cp $BASE/nvim/lua/colors/black.lua $BASE/nvim/lua/colors/init.lua
+
+    # Wallpaper
+    hyprctl hyprpaper preload /home/allan/Downloads/images/library.webp
+    hyprctl hyprpaper wallpaper "eDP-1,/home/allan/Downloads/images/library.webp"
+    hyprctl hyprpaper wallpaper "DP-1,/home/allan/Downloads/images/library.webp"
+    hyprctl hyprpaper wallpaper "HDMI-A-1,/home/allan/Downloads/images/library.webp"
 fi
 
-
-#mpvpaper eDP-1 /home/allan/Downloads/videos/chasca-genshin-impact.3840x2160.mp4 -o "loop=inf video-unscaled=no video-zoom=0.4"
+# Restart waybar
 pkill waybar
 nohup waybar >/dev/null 2>&1 &
 exec >/dev/null 2>&1
